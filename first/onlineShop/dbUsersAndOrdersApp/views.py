a3d49d18e8edd90a55320e11139a1c5be18b2511
Denowsky <denowsky26@yahoo.com>
2024-02-21 09:52:38 +0300

    изменена модель, создана форма редактирования товара, добавлена возможность подгружать фотографии

--- first/onlineShop/dbUsersAndOrdersApp/views.py
+++ first/onlineShop/dbUsersAndOrdersApp/views.py
@@ -1,41 +1,65 @@
-from datetime import datetime, date, time, timedelta
+from datetime import datetime, timedelta
 from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Order, Client
-import pandas as pd
+from django.shortcuts import get_object_or_404, render
+from .models import Order, Client, Product
+from .forms import ProductForm
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 
 def index(request):
     return render(request, "dbUsersAndOrdersApp/index.html")
 
-def client(request):
-    if request.method == "POST":
-        pk = request.POST.get("client_id")
-        client = Client.objects.filter(pk=pk).first()
-        if client is not None:
-            context = {
-                "client" : client.name,
-                "pk" : pk
-            }
-            return render(request, "dbUsersAndOrdersApp/client.html", context)
-        else:
-            return HttpResponse("Такого пользователя нет в базе данных")
 
-def by_period(request, pk, days):
+def by_period(request):
+    pk = request.POST.get("client_id")
     client = Client.objects.filter(pk=pk).first()
-    seven_days_ago = datetime.now() - timedelta(days=days)
-    orders = Order.objects.filter(client=client, date_ordered__gte = seven_days_ago)
-    orders_dict = {}
-    for order in orders:
-        products = []
-        for product in order.products.all():
-            products.append(str(product))
-        orders_dict[order.date_ordered] = products
+    periods = [7, 30, 365]
+    result = {}
+    
+    for period in periods:
+        days = datetime.now() - timedelta(days=period)
+        orders = Order.objects.filter(client=client, date_ordered__gte=days)
+        
+        orders_dict = {}
+        for order in orders:
+            products = [str(product) for product in order.products.all()]
+            orders_dict[order.date_ordered] = products
+        result[period] = orders_dict
+    
     context = {
-          'period' : days,
-          'client' : client.name,
-          'count': len(orders),
-          'orders' : orders_dict
-        }
-    return render(request, "dbUsersAndOrdersApp/filter.html", context)
+        'client': client.name,
+        'orders': result
+    }
+    
+    return render(request, "dbUsersAndOrdersApp/orders.html", context)
+
+def product_form(request):
+    if request.method == 'POST':
+        try: 
+            pk = request.POST.get("product_id")
+            product = get_object_or_404(Product, pk=pk)
+        except Exception:
+            return HttpResponse("Такого товара нет в базе данных")
+        form = ProductForm(instance=product)
+        return render(request, 'dbUsersAndOrdersApp/product.html', {'form': form, 'product_id' : pk})
+    
+def save_product(request, pk):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        product = Product.objects.get(pk=pk)
+        if form.is_valid():
+            product.name = form.cleaned_data['name']
+            product.description = form.cleaned_data['description']
+            product.price = form.cleaned_data['price']
+            product.count = form.cleaned_data['count']
+            product.image = form.cleaned_data['image']
+
+            try:
+                fs = FileSystemStorage()
+                filename = fs.save(product.image.name, product.image)
+                product.image = filename
+                product.save()
+                return HttpResponse("Продукт сохранён")
+            except Exception as e:
+                return HttpResponse("Произошла ошибка при сохранении продукта")
